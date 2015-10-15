5962a4a043ee749be4ddeea9efe80c4118d9b43b
ZQ <hustzq06@gmail.com>
2015-10-16 07:22:02 +0800

    check whether the change will be pushed to the github

--- simpleFM/ViewController.swift
+++ simpleFM/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("hello")
+        //just check whether the project can be push the repository
         // Do any additional setup after loading the view, typically from a nib.
     }
 
